efcde7e5d50cdb8799e955c8b8f78ab660ded424
Daniel Inkpen <dan2552@gmail.com>
2019-02-20 21:45:23 +0000

    Keep output of previous command in $output ruby variable

--- app/hedgehog/execution/binary.rb
+++ app/hedgehog/execution/binary.rb
@@ -12,8 +12,27 @@ module Hedgehog
           # e.g `$(exit 12); echo $?` will print 12
           set_previous_status = "$(exit #{$?&.exitstatus || 0}); "
 
-          pid = Process.spawn(set_previous_status + command.with_binary_path)
-          Process.wait(pid)
+          # Allow recording of session characters. Allowing both color
+          # characters and interactive sessions to be recorded.
+          make_typescript_of_terminal_session = "script -q /dev/null "
+
+          # Pipe to terminal while we're taking the original stdout/stderr
+          # output into our io_pipe object.
+          pipe_to_terminal = " | tee /dev/tty"
+
+          to_execute = set_previous_status +
+            make_typescript_of_terminal_session +
+            command.with_binary_path +
+            pipe_to_terminal
+
+          io_r, io_w = IO.pipe
+          pid = Process.spawn(to_execute, out: io_w)
+          io_w.close
+          Process.wait
+
+          $output = io_r.read
+
+          io_r.close
         rescue Interrupt
           Process.kill("INT", pid)
           Process.wait(pid)
