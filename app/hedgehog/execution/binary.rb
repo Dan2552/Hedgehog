cead45dc55b5b066149fb0629a8b844cafe546ea
Daniel Inkpen <dan2552@gmail.com>
2019-05-30 19:12:09 +0100

    Keeping output

--- app/hedgehog/execution/binary.rb
+++ app/hedgehog/execution/binary.rb
@@ -9,10 +9,52 @@ module Hedgehog
         # To set the $? variable for the process, we first run an exit.
         #
         # e.g `$(exit 12); echo $?` will print 12
-        set_previous_status = "$(exit #{$?&.exitstatus || 0}); "
+        system("exit #{$?&.exitstatus || 0}")
 
-        system(set_previous_status + command.with_binary_path)
+        # ----------------------
+        # SYSTEM - problems: can't see output, can't print newline if there isn't one
+        # system(command.with_binary_path)
+        # print "\n"
 
+        # ----------------------
+        # OPEN3 - problems: no color
+        # output = ""
+        # require 'open3'
+        # Open3.popen2e(command.with_binary_path) do |stdin, stdout_and_err, wait_thr|
+        #   puts "???"
+        #   while (char = stdout_and_err.getc)
+        #     print char
+        #     output += char
+        #   end
+        #   print(c)
+        #   output += c
+        # end
+
+        # ----------------------
+        # PTY - problems: irb doesn't work properly
+
+        output = ""
+        require 'pty'
+        PTY.spawn(command.with_binary_path) do |stdout, stdin, pid|
+          stdin.close
+          begin
+            while (char = stdout.getc)
+              print char
+              output += char
+            end
+            # stdout.each { |line| print line }
+          rescue Errno::EIO
+          ensure
+            Process.wait(pid)
+          end
+        end
+
+        print "⏎\r\n" unless output.end_with?("\r\n") || output.empty?
+
+        Hedgehog::Execution::Ruby::Binding
+          .shared_instance
+          ._binding
+          .local_variable_set(:_, output)
       rescue Interrupt
       ensure
         Process.wait rescue SystemCallError
