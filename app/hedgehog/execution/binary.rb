2120e66e41598ea6c63007e3f70f5a95de4fb6bd
Daniel Inkpen <dan2552@gmail.com>
2019-06-01 00:19:30 +0100

    Cleanup

--- app/hedgehog/execution/binary.rb
+++ app/hedgehog/execution/binary.rb
@@ -13,67 +13,6 @@ module Hedgehog
 
         to_execute = set_previous_status + command.with_binary_path
 
-        # ----------------------
-        # SYSTEM - problems: can't see output, can't print newline if there isn't one
-        # system(command.with_binary_path)
-        # print "\n"
-
-        # ----------------------
-        # OPEN3 - problems: no color
-        # output = ""
-        # require 'open3'
-        # Open3.popen2e(command.with_binary_path) do |stdin, stdout_and_err, wait_thr|
-        #   puts "???"
-        #   while (char = stdout_and_err.getc)
-        #     print char
-        #     output += char
-        #   end
-        #   print(c)
-        #   output += c
-        # end
-
-        # ----------------------
-        # PTY - problems:
-        # irb doesn't work properly - SOLVED by thread passing characters into stdin
-        # long outputs take forever to print
-        # needs separate thread handling stdin
-
-        # output = ""
-        # require 'pty'
-        # require "io/console"
-
-        # Hedgehog::Terminal.silence!
-        # PTY.spawn(to_execute) do |stdout_and_err, stdin, pid|
-        #   stdout_and_err.winsize = $stdout.winsize
-        #   Signal.trap(:WINCH) do
-        #     stdout_and_err.winsize = $stdout.winsize
-        #   end
-        #   thread = Thread.new(stdin) do |terr|
-        #     while true
-        #       char = STDIN.read(1)
-        #       stdin << char
-        #     end
-        #   end
-        #   begin
-        #     while (char = stdout_and_err.getc)
-        #       print char
-        #       output += char
-        #     end
-        #   rescue Errno::EIO
-        #   ensure
-        #     thread.kill
-        #     Process.wait(pid)
-        #     Hedgehog::Terminal.restore!
-        #   end
-        # end
-
-        # -------------
-        # PTY 2
-        # problems:
-        # - printing characters is slow (i think we're stuck with it though).
-
-        start_time = Time.now.to_f
-
         output = ""
         require 'pty'
         require 'io/console'
@@ -82,15 +21,9 @@ module Hedgehog
 
         pid = Process.spawn(to_execute, :in => STDIN, [:out, :err] => slave)
         slave.close
-
         master.winsize = $stdout.winsize
-        Signal.trap(:WINCH) do
-          master.winsize = $stdout.winsize
-        end
-
-        Signal.trap "SIGINT" do
-          Process.kill("INT", pid)
-        end
+        Signal.trap(:WINCH) { master.winsize = $stdout.winsize }
+        Signal.trap(:SIGINT) { Process.kill("INT", pid) }
 
         master.each_char do |char|
           STDOUT.print char
@@ -100,34 +33,8 @@ module Hedgehog
         Process.wait(pid)
         master.close
 
-# -----------------------
-
-        # IO.popen(to_execute, :in => STDIN, :out => slave, err: [:child, :out]) do |_|
-        #   slave.close
-        #   master.winsize = $stdout.winsize
-        #   Signal.trap(:WINCH) do
-        #     master.winsize = $stdout.winsize
-        #   end
-
-        #   master.each_char do |char|
-        #     STDOUT.print char
-        #     output.concat(char)
-        #   end
-        # end
-
-        # puts "hello?"
-
-
-# ------------------
-
-
-        # Process.spawn(to_execute, out: slave, err: [:child, :out])
-
-
-
-
         print "⏎\r\n" unless output.end_with?("\r\n") || output.empty?
-        puts Time.now.to_f - start_time
+
         Hedgehog::Execution::Ruby::Binding
           .shared_instance
           ._binding
