e44291ca1b60188c34d1a6a7c293ca76d0b48ca5
Daniel Inkpen <dan2552@gmail.com>
2019-09-01 00:05:54 +0100

    Start of tokeniser

---
+++ app/hedgehog/parse/tokenizer.rb
@@ -0,0 +1,135 @@
+module Hedgehog
+  module Parse
+    class Token
+      def initialize(type, text)
+        @type = type
+        @text = text
+      end
+
+      attr_reader :type
+      attr_reader :text
+    end
+
+    # Converts a text into a list of tokens for parsing.
+    #
+    class Tokens
+      ALL = [
+        WORD_STARTING_WITH_LETTER = 0,
+
+      ]
+
+      def initialize(text)
+        @text = text.strip
+        @tokens = []
+        @current_state = :empty
+        @current_word = ""
+      end
+
+      def tokenize
+        puts "\n\nSTART----------------"
+        text.each_char do |c|
+          @current_char = c
+          handle_char
+        end
+        handle_end
+        puts "\n\nENDED----------------"
+        @tokens
+      end
+
+      private
+
+      attr_reader :text
+      attr_reader :current_word
+      attr_reader :current_state
+      attr_reader :current_char
+
+      def inspect
+        tokens = @tokens.map(&:type).map(&:to_s).join(",")
+        "[#{tokens}]"
+      end
+
+      def handle_char
+        puts "\ncharacter #{current_char}"
+        case current_state
+        when :empty
+          handle_empty
+        when :word_starting_with_letter, :word_starting_with_number
+          handle_word
+        when :number
+          handle_number
+        else
+          raise ":( handle_char: #{current_state}"
+        end
+        @current_word += current_char
+      end
+
+      def handle_end
+        puts "  handling end"
+
+        add_token(current_state, current_word) if current_state != :empty
+      end
+
+      SINGLE_CHAR_TOKENS = {
+        " " => :space,
+        "=" => :equals,
+        "'" => :single_quote,
+        "`" => :backtick,
+        "\"" => :double_quote,
+        "|" => :pipe
+      }
+
+      def handle_empty
+        puts "  handling empty"
+
+        @current_word = ""
+
+        case current_char
+        when /[a-zA-Z]/
+          @current_state = :word_starting_with_letter
+        when /\d/
+          @current_state = :number
+        when *SINGLE_CHAR_TOKENS.keys
+          add_token(SINGLE_CHAR_TOKENS[current_char], current_char)
+        else
+          raise ":( handle_empty: #{current_char}"
+        end
+      end
+
+      def handle_word
+        puts "  handling word"
+        case current_char
+        when *SINGLE_CHAR_TOKENS.keys
+          end_word
+        when /[a-zA-Z]/
+        when /\d/
+        else
+          raise ":( handle_word: #{current_char}"
+        end
+      end
+
+      def handle_number
+        puts "  handling number"
+        case current_char
+        when /\d/
+        when /[a-zA-Z]/
+          @current_state = :word_starting_with_number
+        else
+          raise ":( handle_number: #{current_char}"
+        end
+      end
+
+      def end_word
+        add_token(current_state, current_word)
+
+        @current_state = :empty
+
+        handle_empty
+      end
+
+      def add_token(type, word)
+        puts "    + adding token #{type} (#{word})"
+        @tokens << Token.new(type, word)
+      end
+    end
+  end
+end
---
+++ spec/hedgehog/parse/tokens_spec.rb
@@ -0,0 +1,231 @@
+RSpec.describe Hedgehog::Parse::Tokens do
+  let(:described_instance) { described_class.new(text) }
+
+  fdescribe "#tokenize" do
+    let(:text) { "" }
+    subject { described_instance.tokenize }
+
+    describe "a simple command" do
+      let(:text) { "ls" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(1)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("ls")
+      end
+    end
+
+    describe "surrounding whitespace" do
+      let(:text) { "  \n  ls  \n " }
+
+      it "doesn't count towards extra tokens" do
+        expect(subject.count).to eq(1)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("ls")
+      end
+    end
+
+    describe "a command with a single argument" do
+      let(:text) { "echo hello" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("echo")
+
+        expect(subject[1].type).to eq(:space)
+        expect(subject[1].text).to eq(" ")
+
+        expect(subject[2].type).to eq(:word_starting_with_letter)
+        expect(subject[2].text).to eq("hello")
+      end
+    end
+
+    describe "a command with multiple arguments" do
+      let(:text) { "echo hello world" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(5)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("echo")
+
+        expect(subject[1].type).to eq(:space)
+        expect(subject[1].text).to eq(" ")
+
+        expect(subject[2].type).to eq(:word_starting_with_letter)
+        expect(subject[2].text).to eq("hello")
+
+        expect(subject[3].type).to eq(:space)
+        expect(subject[3].text).to eq(" ")
+
+        expect(subject[4].type).to eq(:word_starting_with_letter)
+        expect(subject[4].text).to eq("world")
+      end
+    end
+
+    describe "numbers" do
+      let(:text) { "123" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(1)
+        expect(subject[0].type).to eq(:number)
+        expect(subject[0].text).to eq("123")
+      end
+    end
+
+    describe "word starting with letter with numbers" do
+      let(:text) { "abc123" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(1)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("abc123")
+      end
+    end
+
+    describe "word starting with a number" do
+      let(:text) { "123abc" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(1)
+        expect(subject[0].type).to eq(:word_starting_with_number)
+        expect(subject[0].text).to eq("123abc")
+      end
+    end
+
+    describe "equals (like env vars) with numbers as value" do
+      let(:text) { "abc=123" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("abc")
+
+        expect(subject[1].type).to eq(:equals)
+        expect(subject[1].text).to eq("=")
+
+        expect(subject[2].type).to eq(:number)
+        expect(subject[2].text).to eq("123")
+      end
+    end
+
+    describe "equals (like env vars) with word starting with number as value" do
+      let(:text) { "abc=123a" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("abc")
+
+        expect(subject[1].type).to eq(:equals)
+        expect(subject[1].text).to eq("=")
+
+        expect(subject[2].type).to eq(:word_starting_with_number)
+        expect(subject[2].text).to eq("123a")
+      end
+    end
+
+    describe "equals (like env vars) with word starting with letter as value" do
+      let(:text) { "abc=a123" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("abc")
+
+        expect(subject[1].type).to eq(:equals)
+        expect(subject[1].text).to eq("=")
+
+        expect(subject[2].type).to eq(:word_starting_with_letter)
+        expect(subject[2].text).to eq("a123")
+      end
+    end
+
+    describe "equals with spaces" do
+      let(:text) { "abc = 123" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(5)
+        expect(subject[0].type).to eq(:word_starting_with_letter)
+        expect(subject[0].text).to eq("abc")
+
+        expect(subject[1].type).to eq(:space)
+        expect(subject[1].text).to eq(" ")
+
+        expect(subject[2].type).to eq(:equals)
+        expect(subject[2].text).to eq("=")
+
+        expect(subject[3].type).to eq(:space)
+        expect(subject[3].text).to eq(" ")
+
+        expect(subject[4].type).to eq(:number)
+        expect(subject[4].text).to eq("123")
+      end
+    end
+
+    describe "single quote" do
+      let(:text) { "'hello'" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+
+        expect(subject[0].type).to eq(:single_quote)
+        expect(subject[0].text).to eq("'")
+
+        expect(subject[1].type).to eq(:word_starting_with_letter)
+        expect(subject[1].text).to eq("hello")
+
+
+        expect(subject[2].type).to eq(:single_quote)
+        expect(subject[2].text).to eq("'")
+      end
+    end
+
+    describe "double quote" do
+      let(:text) { "\"hello\"" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+
+        expect(subject[0].type).to eq(:double_quote)
+        expect(subject[0].text).to eq("\"")
+
+        expect(subject[1].type).to eq(:word_starting_with_letter)
+        expect(subject[1].text).to eq("hello")
+
+
+        expect(subject[2].type).to eq(:double_quote)
+        expect(subject[2].text).to eq("\"")
+      end
+    end
+
+    describe "backtick" do
+      let(:text) { "`hello`" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(3)
+
+        expect(subject[0].type).to eq(:backtick)
+        expect(subject[0].text).to eq("`")
+
+        expect(subject[1].type).to eq(:word_starting_with_letter)
+        expect(subject[1].text).to eq("hello")
+
+
+        expect(subject[2].type).to eq(:backtick)
+        expect(subject[2].text).to eq("`")
+      end
+    end
+
+    describe "pipe" do
+      let(:text) { "|" }
+
+      it "returns the tokens" do
+        expect(subject.count).to eq(1)
+
+        expect(subject[0].type).to eq(:pipe)
+        expect(subject[0].text).to eq("|")
+      end
+    end
+  end
+end
